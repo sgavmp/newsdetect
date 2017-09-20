538747938215a6ff6071d6ff0546791a048f6156
Sergio Garcia <sga.vmp@gmail.com>
2017-09-20 20:32:01 +0200

    Models

---
+++ newsdetect-main/src/main/java/main/NewsdetectApplication.java
@@ -0,0 +1,12 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsdetectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsdetectApplication.class, args);
+	}
+}
